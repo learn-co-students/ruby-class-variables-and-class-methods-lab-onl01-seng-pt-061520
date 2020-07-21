a4ce0b21a404e3b4b24121e65cae33a757bdee1f
Amyre Polk <myremyre101@icloud.com>
2020-07-21 20:12:16 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,52 @@
+class Song 
+    attr_accessor :name, :artist, :genre
+
+    @@count = 0
+    @@artists = []
+    @@genres = []
+
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+       @@genres << @genre
+       @@artists << @artist
+       @@count += 1
+    end
+
+    def self.count
+        @@count
+    end
+
+    def self.genres
+        @@genres.uniq
+    end
+
+    def self.artists
+        @@artists.uniq
+    end
+
+    def self.genre_count
+        genre_count = {}
+        @@genres.each do |genre|
+            if genre_count[genre]
+                genre_count[genre] += 1
+            else
+                genre_count[genre] = 1
+            end
+        end
+        genre_count
+     end
+
+     def self.artist_count
+        artist_count = {}
+        @@artists.each do |artists|
+            if artist_count[artists]
+                artist_count[artists] += 1
+            else
+                artist_count[artists] = 1
+            end
+        end
+        artist_count
+    end
+end
